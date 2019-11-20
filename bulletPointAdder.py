3471fde40987df016b4e7f7c4c623661f20c3c0d
jacebkey <jacebkey@gmail.com>
2019-11-19 23:49:42 -0600

    updated bulletpointadder

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -7,6 +7,7 @@ text = pyperclip.paste()
 
 # Separate lines and add stars.
 lines = text.split('\n')
+print(lines)
 for i in range(len(lines)): #loop through all idnexes in yhe "lines" list
     lines[i] = "* " + lines[i] # add star to each string in "line" list
 text = '\n'.join(lines)
